cbc39ccdfbdeb3442ad96e40568c418e5c477769
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-05-01 11:27:46 -0400

    add verify jwt

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { index, create, destroy, update, login } = require('../controllers/user.controller');
+const verifyJWT = require('../utils/verifyJWT');
 
 const routerUser = express.Router();
 
 routerUser.route('/')
-    .get(index)
+    .get(verifyJWT, index)
     .post(create);
 
 routerUser.route('/login')
     .post(login);
 
 routerUser.route('/:id')
-    .delete(destroy)
-    .put(update);
+    .delete(verifyJWT, destroy)
+    .put(verifyJWT, update);
 
 module.exports = routerUser;
