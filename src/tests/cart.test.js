16f2c97df2f2b9dd7d9be08ce4913f31223c10eb
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-05-05 15:37:40 -0400

    [wip] cart.test

--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -4,3 +4,28 @@ const request = require('supertest');
 const app = require('../app');
 
 const URL_BASE = '/api/v1/cart';
+
+let TOKEN;
+
+beforeAll(async () => {
+    const user = {
+        email: 'josuan.rodriguez@mail.com',
+        password: 'josuan1234',
+    };
+
+    const response = await request(app)
+        .post('/api/v1/users/login')
+        .send(user);
+
+    TOKEN = response.body.token;
+});
+
+test('GET "/cart" should return status code 200', async () => {
+    const response = await request(app)
+        .get(URL_BASE)
+        .set('Authorization', `Bearer ${TOKEN}`);
+    
+    console.log(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+});
