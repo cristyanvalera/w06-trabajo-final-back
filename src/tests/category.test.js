c8fb3d492b9cf60d3dd61c2d397a22c81f1ca92a
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-05-05 14:00:00 -0400

    wip

--- src/tests/category.test.js
+++ src/tests/category.test.js
@@ -1,3 +1,5 @@
+require('../models');
+
 const request = require('supertest');
 const app = require('../app');
 
@@ -21,7 +23,7 @@ beforeAll(async () => {
 test('GET "/categories" should return status code 200', async () => {
     const response = await request(app)
         .get(URL_BASE);
-    
+    console.log(response.body)
     expect(response.status).toBe(200);
     expect(response.body).toBeDefined();
 });
